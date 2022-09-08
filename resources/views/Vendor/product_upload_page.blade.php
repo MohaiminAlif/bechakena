1a2fa759b94f3730c85a2ee9c845f3fc11d68382
Mohaimin Alif <69976918+MohaiminAlif@users.noreply.github.com>
2022-09-08 12:00:59 +0600

    product insert updated with 'content'

--- resources/views/Vendor/product_upload_page.blade.php
+++ resources/views/Vendor/product_upload_page.blade.php
@@ -1,43 +1,18 @@
-@extends('layouts.app')
+@extends('layouts.app') @section('title') Product Upload @endsection 
 
 @section('content')
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Product Insert</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.ckeditor.com/ckeditor5/35.1.0/classic/ckeditor.js"></script>
-
-</head>
-
-<body>
 
 
-    <div class="row">
 
-        <div class="col-2 userCategory">
-
-            <div class="row mt-2">
-                <div class="col"><a href="{{route('home')}}"><button class="btn">Profile</button></a></div>
-            </div>
-            <div class="row mt-2">
-                <div class="col"><a href="{{route('vendor')}}"><button class="btn">Vendor</button></a></div>
-            </div>
-
-        </div>
-
-        <div class="col-9 container">
+    <div class="row container"> 
     
             <div>
-                <div class="row offset-3">
+                <div class="row text-center">
                     <div class="col">
-                        <h2 class="">Product Insert Page</h2>
+                        <h2 class="text-primary">Product Insert Page</h2>
                     </div>
                     
                 </div>
@@ -106,11 +81,7 @@
         </div>
     </div>
 
-    <script src="{{asset('/')}}assets/js/jquery-3.6.1.min.js"></script>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-    
+    <script src="https://cdn.ckeditor.com/ckeditor5/35.1.0/classic/ckeditor.js"></script>
                 <script>
                         ClassicEditor
                                 .create( document.querySelector( '#editor' ) )
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <!-- Bootstrap CSS -->
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <!-- Custom CSS -->
@@ -186,6 +187,8 @@
     </div>
     <!-- Footer End -->
 
+    
+    <script src="{{asset('/')}}assets/js/jquery-3.6.1.min.js"></script>
     <script src="https://kit.fontawesome.com/ca76eb4afc.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
